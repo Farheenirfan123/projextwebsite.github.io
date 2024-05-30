3e8b59b9096e8034db61a4be21c0bd217a6ec129
Farheen123 <farheenirfan12nov@gmail.com>
2024-05-30 07:13:35 -0700

    add files

--- srcipt.js
+++ srcipt.js
@@ -1,5 +1,5 @@
 // document.querySelector('nav').style.backgroundColor = "white"
-addEventListener('scroll', ()=> {
-document.querySelector('nav').style.backgroundColor = "white"
-
-} )
+// addEventListener('scroll', ()=> {
+// document.querySelector('nav').style.backgroundColor = "white"
+// // document.getElementsByClassName('.nav-link').style.color = 'black'
+// } )
